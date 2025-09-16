8aa0295ac0b908f41c1890b67e00da8052962e19
KeithCoreDumped <2385402800@qq.com>
2025-09-17 01:42:03 +0800

    add @types/mdx dependency; fix 'defined but never used' warning

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+// import { usePathname } from "next/navigation";
 import {FileText, Home, Package, Users} from "lucide-react";
 import Image from "next/image";
 import {CollapsibleNav} from "@/components/layout/CollapsibleNav";
